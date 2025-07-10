890d09ea290c29968adda60ebf9ce0145bda276e
Jesús Valeta <jvaleta1995@gmail.com>
2025-07-10 14:59:40 -0500

    Funciones de orden superior

---
+++ src/fOrdenSuperior004/OrdenSuperior.java
@@ -0,0 +1,35 @@
+package fOrdenSuperior004;
+
+import interfacesFuncionales003.Operaciones;
+import interfacesFuncionales003.Validador;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class OrdenSuperior {
+
+    public static void main(String[] args) {
+
+        int resultado = operar(3, 7, (a, b) -> a * b);
+        System.out.println(resultado);
+
+        boolean emailValido =  validar("valdoring@gmail.com", email-> email.contains("@"));
+        System.out.println(emailValido);
+
+        Function<Integer, Integer> porCuatro = multiplicador(4);
+        System.out.println(porCuatro.apply(28));
+    }
+
+    static int operar(int a, int b, Operaciones operaciones){
+        return operaciones.calcular(a, b);
+    }
+
+    static boolean validar(String valor, Validador<String> validar){
+        return validar.validar(valor);
+    }
+
+    static Function<Integer, Integer> multiplicador(int factor){
+        return x -> x * factor;
+    }
+}
